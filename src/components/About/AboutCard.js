8450fad81b94f99d11254cabc51a0e39ac0709b7
Sephens <adesephens@gmail.com>
2023-03-04 22:06:12 +0300

    scond commit

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -25,6 +25,9 @@ function AboutCard() {
             <li className="about-activity">
               <ImPointRight /> Travelling
             </li>
+            <li className="about-activity">
+              <ImPointRight /> Reading
+            </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
