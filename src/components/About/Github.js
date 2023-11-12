93573d2311f7acf4e1890e144fbe278d7b4c322e
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2023-11-12 23:10:10 +0300

    Update Github.js

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -12,7 +12,7 @@ function Github() {
         username="Sephens"
         blockSize={15}
         blockMargin={5}
-        color="#c084f5"
+        color="#14d31e"
         fontSize={16}
       />
     </Row>
