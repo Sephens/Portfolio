ea2d890b3326edc5132b99b0b1c19446b5db86e2
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2024-01-29 05:45:54 +0000

    ...4

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/test.svg";
+import homeLogo from "../../Assets/Hand coding-bro.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
