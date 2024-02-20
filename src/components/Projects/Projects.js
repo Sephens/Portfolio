8b777b9aa1ab022dd968b984fe59b63e50f6a54c
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2024-02-20 22:18:59 +0300

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -18,7 +18,7 @@ export default function Projects() {
           My Recent <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+          Here are a few projects I'm working on or thinking of working on.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
