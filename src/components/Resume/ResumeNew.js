f0f41113d7067993889f1d8d86cfadcc924178b2
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2024-09-09 14:43:24 +0300

    992024

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/Sephens/Portfolio/master/src/Assets/mycv.pdf";
+  "https://raw.githubusercontent.com/Sephens/Portfolio/master/src/Assets/cv.pdf";
 
 export default function ResumeNew() {
   const [numPages, setNumPages] = useState(null);
